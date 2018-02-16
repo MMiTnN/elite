3de7b7a76bc8eccbffae0f835bd4ebd22669c000
MMiTnN <mmitnn@gmail.com>
2018-02-16 22:47:31 +0700

    Update nav

--- wp-content/themes/elite/layouts/nav.php
+++ wp-content/themes/elite/layouts/nav.php
@@ -30,8 +30,36 @@
           
         </div>
          <div id="navbarMenuHeroA" class="navbar-menu">
-          <div class="navbar-end">
+          <div class="navbar-end is-hidden-touch">
             <?php $header_menu = wp_get_menu_array('Header Menu');?>
+            <?php foreach ($header_menu as $menu): 
+                if(!empty($menu['children'])): ?>
+                <div class="navbar-item has-dropdown">
+                  <a class="navbar-link" href="<?php echo $menu['url']?>" >
+                    <?php echo $menu['title']?>
+                  </a>
+
+                  <div class="navbar-dropdown is-boxed">
+                    <?php foreach ($menu['children'] as $key => $value): ?>
+                      <a class="navbar-item" href="<?php echo $value['url'] ?>">
+                        <?php echo $value['title'] ?>
+                      </a>     
+                     <?php endforeach; ?>  
+                  </div>
+                </div>
+            <?php else: ?>
+              <a class="navbar-item" href="<?php echo $menu['url']?>">
+                <?php echo $menu['title']; ?>
+              </a>
+            <?php endif; 
+            endforeach; ?>
+            <a href="" id="lang-switcher"  style="cursor: pointer; cursor: hand;" class="navbar-item lang hidden-xs hidden-sm"><p><?php echo check_msls() == 'us' ? 'EN' : 'TH'; ?></p></a>
+            <div class="line-solid"></div>
+            <a id="xs-eng-lang" class="visible-xs visible-sm navbar-item" href="">English</a>
+            <a id="xs-th-lang" class="visible-xs visible-sm navbar-item" href="">ภาษาไทย</a>
+          </div>
+          <div class="navbar-end is-hidden-desktop">
+            <?php $header_menu = wp_get_menu_array('Footer Menu');?>
             <?php foreach ($header_menu as $menu): 
                 if(!empty($menu['children'])): ?>
                 <div class="navbar-item has-dropdown">
