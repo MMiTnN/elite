fba928c0f16bfb918af1e8d6c6f033bbdea9ab6d
MMiTnN <mmitnn@gmail.com>
2018-02-18 23:01:12 +0700

    Update clearflix

--- wp-content/themes/elite/archive-events.php
+++ wp-content/themes/elite/archive-events.php
@@ -39,7 +39,9 @@ $events = $posts->posts;
         <section class="projects-list">
             <div class="container-fluid">
                 <div class="row">
-                    <?php foreach($events as $pro): 
+                    <?php
+                     $count = 1; 
+                    foreach($events as $pro): 
                     $post_id = $pro->ID;
                     /* Get post thumnail */
                     $image_id = get_post_thumbnail_id($post_id);
@@ -50,17 +52,25 @@ $events = $posts->posts;
                         $image = $image_arr[0];
                     }
                     ?>
-                     <a class="img-pro" href="<?php echo get_permalink($post_id); ?>">
+                     
                         <div class="col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 20px;">
-                            
+                            <a class="img-pro" href="<?php echo get_permalink($post_id); ?>">
                             <div class="block black"  style="background-image: url(<?php echo $image ?>); background-size: cover;
                                                                                                           background-repeat: no-repeat;
                                                                                                           background-position: 55% 55%;" >
                             </div>
                             <h5> <?php echo $pro->post_title; ?></h5>
+                            </a>
                         </div>
-                     </a>
+                     
                     <?php
+                    if(($count%3==0)): ?>
+                      <div class="clearfix visible-sm"></div>
+                    <?php endif; ?>
+                    <?php if(($count%4==0)): ?>
+                      <div class="clearfix visible-md visible-lg"></div>
+                    <?php endif; ?>
+                    <?php $count++;
                     endforeach; ?>
                 </div>
             </div>
--- wp-content/themes/elite/archive-projects.php
+++ wp-content/themes/elite/archive-projects.php
@@ -56,7 +56,7 @@ $projects = $posts->posts;
             <div class="container-fluid">
                 <div class="row">
                     <?php 
-                    $count = 0;
+                    $count = 1;
                     foreach($projects as $pro): 
                     $post_id = $pro->ID;
                     /* Get post thumnail */
@@ -68,18 +68,21 @@ $projects = $posts->posts;
                         $image = $image_arr[0];
                     }
                     ?>
+                        <div class="col-md-3 col-sm-4 col-xs-12" style="margin-bottom: 50px;">
                      <a class="img-pro" href="<?php echo get_permalink($post_id); ?>">
-                        <div class="col-md-3 col-sm-4 col-xs-12" style="padding-bottom: 50px;">
-                            
                             <div class="block black"  style="background-image: url(<?php echo $image ?>); background-size: cover;
                                                                                                           background-repeat: no-repeat;
                                                                                                           background-position: 55% 55%;" >
                             </div>
                             <h5> <?php echo $pro->post_title; ?></h5>
-                        </div>
                      </a>
-                     <?php if(($count%3==0) && ($count!= 0)): ?>
-                      <div class="clearfix visible-xs-block"></div>
+                        </div>
+
+                    <?php if(($count%3==0)): ?>
+                      <div class="clearfix visible-sm"></div>
+                    <?php endif; ?>
+                    <?php if(($count%4==0)): ?>
+                      <div class="clearfix visible-md visible-lg"></div>
                     <?php endif; ?>
                     <?php $count++;
                     endforeach; ?>
